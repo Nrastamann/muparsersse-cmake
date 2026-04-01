785f5ee5c6de67f29e928740227130f9e950a838
Nrastamann <nicita351@Yandex.ru>
2026-04-01 18:55:51 +0300

    WIP

--- include/mecUnitTest.h
+++ include/mecUnitTest.h
@@ -38,88 +38,167 @@
 
 namespace mec
 {
-  /** \brief Namespace for test cases. */
-  namespace Test
-  {
-    //------------------------------------------------------------------------------
-    /** \brief Test cases for unit testing.
+/** \brief Namespace for test cases. */
+namespace Test
+{
+//------------------------------------------------------------------------------
+/** \brief Test cases for unit testing.
     */
-    class UnitTest // final
-    {
+class UnitTest // final
+{
     private:
-        // Multiarg callbacks
-        static value_type f1of1(value_type v) { return v;};
-      	
-        static value_type f1of2(value_type v, value_type  ) {return v;};
-        static value_type f2of2(value_type  , value_type v) {return v;};
+	// Multiarg callbacks
+	static value_type f1of1(value_type v)
+	{
+		return v;
+	};
 
-        static value_type f1of3(value_type v, value_type  , value_type  ) {return v;};
-        static value_type f2of3(value_type  , value_type v, value_type  ) {return v;};
-        static value_type f3of3(value_type  , value_type  , value_type v) {return v;};
-      	
-        static value_type f1of4(value_type v, value_type,   value_type  , value_type  ) {return v;}
-        static value_type f2of4(value_type  , value_type v, value_type  , value_type  ) {return v;}
-        static value_type f3of4(value_type  , value_type,   value_type v, value_type  ) {return v;}
-        static value_type f4of4(value_type  , value_type,   value_type  , value_type v) {return v;}
+	static value_type f1of2(value_type v, value_type)
+	{
+		return v;
+	};
+	static value_type f2of2(value_type, value_type v)
+	{
+		return v;
+	};
 
-	      static value_type f1of5(value_type v, value_type,   value_type  , value_type  , value_type  ) { return v; }
-	      static value_type f2of5(value_type  , value_type v, value_type  , value_type  , value_type  ) { return v; }
-	      static value_type f3of5(value_type  , value_type,   value_type v, value_type  , value_type  ) { return v; }
-	      static value_type f4of5(value_type  , value_type,   value_type  , value_type v, value_type  ) { return v; }
-	      static value_type f5of5(value_type  , value_type,   value_type  , value_type  , value_type v) { return v; }
+	static value_type f1of3(value_type v, value_type, value_type)
+	{
+		return v;
+	};
+	static value_type f2of3(value_type, value_type v, value_type)
+	{
+		return v;
+	};
+	static value_type f3of3(value_type, value_type, value_type v)
+	{
+		return v;
+	};
 
-        static value_type Min(value_type a_fVal1, value_type a_fVal2) { return (a_fVal1<a_fVal2) ? a_fVal1 : a_fVal2; }
-  	    static value_type Max(value_type a_fVal1, value_type a_fVal2) { return (a_fVal1>a_fVal2) ? a_fVal1 : a_fVal2; }
+	static value_type f1of4(value_type v, value_type, value_type,
+				value_type)
+	{
+		return v;
+	}
+	static value_type f2of4(value_type, value_type v, value_type,
+				value_type)
+	{
+		return v;
+	}
+	static value_type f3of4(value_type, value_type, value_type v,
+				value_type)
+	{
+		return v;
+	}
+	static value_type f4of4(value_type, value_type, value_type,
+				value_type v)
+	{
+		return v;
+	}
 
-        static value_type plus2(value_type v1) { return v1+2; }
-        static value_type times3(value_type v1) { return v1*3; }
-        static value_type sqr(value_type v1) { return v1*v1; }
-        static value_type sign(value_type v) { return -v; }
-        static value_type add(value_type v1, value_type v2) { return v1+v2; }
-        static value_type and(value_type v1, value_type v2) { return (value_type)((int)v1 & (int)v2); }
-        
-        static value_type Rnd(value_type v)
-        {
-          return (value_type)(1+(v*std::rand()/(RAND_MAX+1.0)));
-        }
+	static value_type f1of5(value_type v, value_type, value_type,
+				value_type, value_type)
+	{
+		return v;
+	}
+	static value_type f2of5(value_type, value_type v, value_type,
+				value_type, value_type)
+	{
+		return v;
+	}
+	static value_type f3of5(value_type, value_type, value_type v,
+				value_type, value_type)
+	{
+		return v;
+	}
+	static value_type f4of5(value_type, value_type, value_type,
+				value_type v, value_type)
+	{
+		return v;
+	}
+	static value_type f5of5(value_type, value_type, value_type, value_type,
+				value_type v)
+	{
+		return v;
+	}
 
-        static value_type Ping()
-        { 
-          return 10; 
-        }
+	static value_type Min(value_type a_fVal1, value_type a_fVal2)
+	{
+		return (a_fVal1 < a_fVal2) ? a_fVal1 : a_fVal2;
+	}
+	static value_type Max(value_type a_fVal1, value_type a_fVal2)
+	{
+		return (a_fVal1 > a_fVal2) ? a_fVal1 : a_fVal2;
+	}
 
-        static int c_iCount;
+	static value_type plus2(value_type v1)
+	{
+		return v1 + 2;
+	}
+	static value_type times3(value_type v1)
+	{
+		return v1 * 3;
+	}
+	static value_type sqr(value_type v1)
+	{
+		return v1 * v1;
+	}
+	static value_type sign(value_type v)
+	{
+		return -v;
+	}
+	static value_type add(value_type v1, value_type v2)
+	{
+		return v1 + v2;
+	}
+	static value_type andd(value_type v1, value_type v2)
+	{
+		return (value_type)((int)v1 & (int)v2);
+	}
 
-	      int TestNames();
-	      int TestSyntax();
-	      int TestPostFix();
-        int TestLogic();
-	      int TestExpression();
-	      int TestInfixOprt();
-	      int TestBinOprt();
-	      int TestVarConst();
-	      int TestInterface();
-	      int TestException();
-        int TestMultiArg();
-        int TestIfThenElse();
+	static value_type Rnd(value_type v)
+	{
+		return (value_type)(1 + (v * std::rand() / (RAND_MAX + 1.0)));
+	}
 
-        void Abort() const;
+	static value_type Ping()
+	{
+		return 10;
+	}
+
+	static int c_iCount;
+
+	int TestNames();
+	int TestSyntax();
+	int TestPostFix();
+	int TestLogic();
+	int TestExpression();
+	int TestInfixOprt();
+	int TestBinOprt();
+	int TestVarConst();
+	int TestInterface();
+	int TestException();
+	int TestMultiArg();
+	int TestIfThenElse();
+
+	void Abort() const;
 
     public:
-        typedef int (UnitTest::*testfun_type)();
+	typedef int (UnitTest::*testfun_type)();
 
-	      UnitTest();
-	      void Run();
+	UnitTest();
+	void Run();
 
     private:
-        std::vector<testfun_type> m_vTestFun;
-	      void AddTest(testfun_type a_pFun);
+	std::vector<testfun_type> m_vTestFun;
+	void AddTest(testfun_type a_pFun);
 
-        // Test Double Parser
-        int EqnTest(const string_type& a_str, value_type a_fRes, bool a_fPass);
-        int ThrowTest(const string_type& a_str, int a_iErrc, bool a_bFail = true);
-    };
-  } // namespace Test
+	// Test Double Parser
+	int EqnTest(const string_type &a_str, value_type a_fRes, bool a_fPass);
+	int ThrowTest(const string_type &a_str, int a_iErrc,
+		      bool a_bFail = true);
+};
+} // namespace Test
 } // namespace mec
 
 #endif
