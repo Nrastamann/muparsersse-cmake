a2957f67b0cb5f586e8080df486d09d666403e3d
Nrastamann <nicita351@Yandex.ru>
2026-04-01 18:48:42 +0300

    WIP

--- include/mecExprCompiler.h
+++ include/mecExprCompiler.h
@@ -30,89 +30,88 @@
 #include <list>
 
 //--- ASMJit compiler---------------------------------------------------------------------
-#include "AsmJit/Assembler.h"
-#include "AsmJit/Compiler.h"
-#include "AsmJit/MemoryManager.h"
-#include "AsmJit/Logger.h"
+#include "Assembler.h"
+#include "Compiler.h"
+#include "MemoryManager.h"
+#include "Logger.h"
 
 //--- muParser framework --------------------------------------------------------------
 #include "mecDef.h"
 #include "mecReversePolishNotation.h"
 #include "mecStack.h"
 
-
 namespace mec
 {
-  class ExprCompiler
-  {
-  public:
-    ExprCompiler();
-    ExprCompiler(const ExprCompiler &ref);
-   ~ExprCompiler();
+class ExprCompiler {
+    public:
+	ExprCompiler();
+	ExprCompiler(const ExprCompiler &ref);
+	~ExprCompiler();
 
-    void Bind(SPackedToken *pRPN);
-    value_type (*Compile(int nHighestReg))();
-    void SetLastRegister(int n);
+	void Bind(SPackedToken *pRPN);
+	value_type (*Compile(int nHighestReg))();
+	void SetLastRegister(int n);
 
-  private:
+    private:
+	struct SJumpTag {
+		SJumpTag(AsmJit::Label *a_Label, int a_StackPos, int a_Len)
+			: Label(a_Label)
+			, StackPos(a_StackPos)
+			, Len(a_Len)
+		{
+		}
 
-    struct SJumpTag
-    {
-      SJumpTag(AsmJit::Label* a_Label, int a_StackPos, int a_Len)
-        :Label(a_Label)
-        ,StackPos(a_StackPos)
-        ,Len(a_Len)
-      {}
+		AsmJit::Label *Label;
+		int StackPos;
+		int Len;
+	};
 
-      AsmJit::Label* Label;
-      int StackPos;
-      int Len;
-    };
+	typedef int int_ptr;
 
-    typedef int int_ptr;
+	SPackedToken *m_pRPN;
+	value_type m_buf;
 
-    SPackedToken *m_pRPN;
-    value_type m_buf;
+	exprfun_type m_pExpr;
+	int m_nStackPos;
+	std::auto_ptr<AsmJit::Compiler> m_pCompiler;
+	AsmJit::XMMRegister xmm[8];
 
-    exprfun_type m_pExpr;
-    int  m_nStackPos;
-    std::auto_ptr<AsmJit::Compiler> m_pCompiler;
-    AsmJit::XMMRegister xmm[8];
+	int PrepareFunctionArguments(int argc);
+	bool LoadArguments(AsmJit::XMMRegister &arg1,
+			   AsmJit::XMMRegister &arg2);
+	bool LoadArgument(AsmJit::XMMRegister &arg);
+	bool LoadArgumentEx(AsmJit::XMMRegister &arg1,
+			    AsmJit::XMMRegister &arg2);
 
-    int PrepareFunctionArguments(int argc);
-    bool LoadArguments(AsmJit::XMMRegister &arg1, AsmJit::XMMRegister &arg2);
-    bool LoadArgument(AsmJit::XMMRegister &arg);
-    bool LoadArgumentEx(AsmJit::XMMRegister &arg1, AsmJit::XMMRegister &arg2);
+	void Push(value_type *v);
 
-    void Push(value_type *v);
+	void GenericFunction(void *pFun, int argc, bool bSaveReg);
+	void BinOp(ECmdCode eCode);
+	void BinOpCompare(ECmdCode eCode);
+	void BinOpLogic(ECmdCode eCode);
+	void PowerOf();
+	void IntrinsicFPUFunction(ECmdCode eCode);
+	void IntrinsicSSEFunction(ECmdCode eCode);
+	void ReleaseExpr();
+	void SaveSSERegisters();
+	void RestoreSSERegisters();
 
-    void GenericFunction(void *pFun, int argc, bool bSaveReg);
-    void BinOp(ECmdCode eCode);
-    void BinOpCompare(ECmdCode eCode);
-    void BinOpLogic(ECmdCode eCode);
-    void PowerOf();
-    void IntrinsicFPUFunction(ECmdCode eCode);
-    void IntrinsicSSEFunction(ECmdCode eCode);
-    void ReleaseExpr();
-    void SaveSSERegisters();
-    void RestoreSSERegisters();
-    
-    void If(int nJumpLen);
-    void Else(int nJumpLen);
-    void EndIf();
+	void If(int nJumpLen);
+	void Else(int nJumpLen);
+	void EndIf();
 
-    void InsertJumpLabels();
+	void InsertJumpLabels();
 
-    // static constants for commonly used values
-    static const value_type c_zero;
-    static const value_type c_one;
-    static const int m_nMXCSR = 0x7e03;
-    //static const int m_nMXCSR = 0x7f85;
-    int m_nLastRegister;
+	// static constants for commonly used values
+	static const value_type c_zero;
+	static const value_type c_one;
+	static const int m_nMXCSR = 0x7e03;
+	//static const int m_nMXCSR = 0x7f85;
+	int m_nLastRegister;
 
-    std::list< SJumpTag > m_vIfLabels;
-    value_type m_xmm_saved[16];
-  };
+	std::list<SJumpTag> m_vIfLabels;
+	value_type m_xmm_saved[16];
+};
 }
 
 #endif
